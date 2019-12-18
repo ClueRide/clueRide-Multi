4fde43fae1bb7d200cf16ddabb5e90b9559881dd
jett <jettmarks@gmail.com>
2019-12-17 20:08:37 -0500

    CI-31 Complete rename of `locEdit` to `ranger`
    Second commit: Move from one directory to the other.

--- projects/ranger/src/app/list/list.page.spec.ts
+++ projects/ranger/src/app/list/list.page.spec.ts
@@ -1,7 +1,7 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
-import { IonicModule } from '@ionic/angular';
+import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {IonicModule} from '@ionic/angular';
 
-import { ListPage } from './list.page';
+import {ListPage} from './list.page';
 
 describe('ListPage', () => {
   let component: ListPage;
