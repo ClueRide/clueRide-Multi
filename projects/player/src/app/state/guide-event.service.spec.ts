d0e1fe456bc632475bef38c890b9bf4fc3079109
jett <jettmarks@gmail.com>
2020-01-10 20:41:40 -0500

    CI-96 Hide "Arrival" button if we're not rolling
    - Adjusts logic for showing the Arrival button.
    
    Also renames the files for the guide-event.service and spec.

--- projects/player/src/app/state/guide-event.service.spec.ts
+++ projects/player/src/app/state/guide-event.service.spec.ts
@@ -1,12 +1,12 @@
 import {TestBed} from '@angular/core/testing';
 
-import {GuideEventServiceService} from './guide-event-service.service';
+import {GuideEventService} from './guide-event.service';
 
 describe('GuideEventServiceService', () => {
   beforeEach(() => TestBed.configureTestingModule({}));
 
   it('should be created', () => {
-    const service: GuideEventServiceService = TestBed.get(GuideEventServiceService);
+    const service: GuideEventService = TestBed.get(GuideEventService);
     expect(service).toBeTruthy();
   });
 });
