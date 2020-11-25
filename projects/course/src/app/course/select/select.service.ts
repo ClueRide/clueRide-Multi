2663518f77ef6d0bf7416cf3a65354eeebd35ec1
jett <jettmarks@gmail.com>
2020-11-24 21:16:02 -0500

    CI-243: Select Course for default
    - Adds button to each Course Line that allows setting the Course to the Default.
    - Adds service that talks to back-end to set the default course.

---
+++ projects/course/src/app/course/select/select.service.ts
@@ -0,0 +1,53 @@
+import {Injectable} from '@angular/core';
+import {Course} from 'cr-lib';
+import {
+  AuthHeaderService,
+  BASE_URL
+} from '../../../../../cr-lib/src/lib/auth/header/auth-header.service';
+import {HttpClient} from '@angular/common/http';
+
+/**
+ * Provides services for choosing the back-end's "default" Course.
+ */
+@Injectable({
+  providedIn: 'root'
+})
+export class SelectService {
+
+  /* Holds our current selection. */
+  selectedCourse: Course = null;
+
+  constructor(
+    public http: HttpClient,
+    private httpService: AuthHeaderService,
+  ) { }
+
+  public isSelected(course: Course): boolean {
+    if (this.selectedCourse) {
+      return course.id === this.selectedCourse.id;
+    } else {
+      return false;
+    }
+  }
+
+  public select(course: Course) {
+    console.log("Making", course.name, "the default Course");
+    this.selectedCourse = course;
+    this.setDefaultCourse(course).subscribe();
+  }
+
+  public reset(course: Course) {
+    this.setDefaultCourse(course).subscribe();
+  }
+
+  public setDefaultCourse(course: Course) {
+    return this.http.post<Course>(
+      BASE_URL + 'course/default',
+      course,
+      {
+        headers: this.httpService.getAuthHeaders()
+      }
+    )
+  }
+
+}
